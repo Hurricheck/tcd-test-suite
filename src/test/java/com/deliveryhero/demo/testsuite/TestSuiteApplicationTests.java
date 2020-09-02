88f880046eb7cefbb8bb0752e0767b5d58ac2562
Pavel Lilishentseu <pavel.lilishentseu@deliveryhero.com>
2020-09-02 14:29:45 +0200

    final touch

--- src/test/java/com/deliveryhero/demo/testsuite/TestSuiteApplicationTests.java
+++ src/test/java/com/deliveryhero/demo/testsuite/TestSuiteApplicationTests.java
@@ -1,19 +1,47 @@
 package com.deliveryhero.demo.testsuite;
 
 import com.deliveryhero.demo.testsuite.util.db.PostgresUtil;
-import org.junit.ClassRule;
+import groovy.util.logging.Slf4j;
+import io.restassured.RestAssured;
 import org.junit.Rule;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.KafkaContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
+@Slf4j
 class TestSuiteApplicationTests {
+	Logger logger = LoggerFactory.getLogger(this.getClass().getName());
+	Slf4jLogConsumer logConsumer = new Slf4jLogConsumer(logger);
+
+	private final static String KAFKA_TOPIC_ENV_NAME = "KAFKA_TOPIC";
+	private final static String KAFKA_CONSUMER_GROUP_ENV_NAME = "KAFKA_CONSUMER_GROUP";
+	private final static String KAFKA_URL_ENV_NAME = "KAFKA_URL";
+
+	private final static String KAFKA_TOPIC_ENV_VALUE = "com.dh.dummy-topic";
+	private final static String KAFKA_CONSUMER_GROUP_ENV_VALUE = "dummy_consumer";
+
+	private final static String POSTGRES_JDBC_URL_ENV_NAME = "POSTGRES_JDBC_URL";
+	private final static String POSTGRES_USERNAME_ENV_NAME = "POSTGRES_USERNAME";
+	private final static String POSTGRES_PASSWORD_ENV_NAME = "POSTGRES_PASSWORD";
+
+	private final static String REQUEST_USER_PARAM = "user";
+	private final static String REQUEST_PASSWORD_HASH_PARAM = "passwordHash";
+	private final static String REQUEST_AUTH_ENDPOINT = "/api/v1/auth";
+
+
 	@Autowired
 	private PostgresUtil postgresUtil;
 
@@ -26,6 +54,12 @@ class TestSuiteApplicationTests {
 			.withUsername("sa")
 			.withPassword("sa");
 
+	private final static String PRODUCER_CONTAINER_NAME = "hurricheck/producer-api";
+	public GenericContainer producer;
+
+	private final static String CONSUMER_CONTAINER_NAME = "hurricheck/consumer";
+	public GenericContainer consumer;
+
 	@BeforeEach
 	public void setUp() {
 		kafka.start();
@@ -37,13 +71,92 @@ class TestSuiteApplicationTests {
 				postgres.getUsername(),
 				postgres.getPassword()
 		);
+		//setup kafka
+		String kafkaURL = kafka.getContainerIpAddress() + ":" + kafka.getMappedPort(9093);
+
+
+		consumer = new GenericContainer(CONSUMER_CONTAINER_NAME)
+				.withEnv(KAFKA_TOPIC_ENV_NAME, KAFKA_TOPIC_ENV_VALUE)
+				.withEnv(KAFKA_URL_ENV_NAME, kafkaURL)
+				.withEnv(KAFKA_CONSUMER_GROUP_ENV_NAME, KAFKA_CONSUMER_GROUP_ENV_VALUE)
+				.withEnv(POSTGRES_JDBC_URL_ENV_NAME, postgres.getJdbcUrl())
+				.withEnv(POSTGRES_USERNAME_ENV_NAME, postgres.getUsername())
+				.withEnv(POSTGRES_PASSWORD_ENV_NAME, postgres.getPassword());
+		consumer.start();
+		consumer.followOutput(logConsumer);
+
+		uglyWait();
+		producer = new GenericContainer(PRODUCER_CONTAINER_NAME)
+				.withEnv(KAFKA_TOPIC_ENV_NAME, KAFKA_TOPIC_ENV_VALUE)
+				.withEnv(KAFKA_URL_ENV_NAME, kafkaURL);
+		producer.addExposedPort(8090);
+		producer.start();
+		producer.followOutput(logConsumer);
+	}
+
+	public String getProducerURL() {
+		return producer.getContainerIpAddress() + ":"+producer.getMappedPort(8090);
+	}
+
+//	@Test
+//	void contextLoads() {
+//		String a = "aaaa";
+//		String b = "bbb";
+//		assertEquals(a, b);
+//	}
+
+	@Test
+	public void restAssuredTestA() {
+		String url = getProducerURL() + REQUEST_AUTH_ENDPOINT;
+		String user = "usernameA";
+		String passwordHash = "afeDWQweqdd124";
+
+		RestAssured
+				.given()
+					.param(REQUEST_USER_PARAM, user)
+					.param(REQUEST_PASSWORD_HASH_PARAM, passwordHash)
+				.get("http://"+ url)
+				.then()
+					.statusCode(200);
+
+		List<Integer> savedIds = postgresUtil.getIDs(user, passwordHash);
+		assertEquals(1, savedIds.size());
+		assertEquals(0, savedIds.get(0));
 	}
 
 	@Test
-	void contextLoads() {
-		String a = "aaaa";
-		String b = "bbb";
-		assertEquals(a, b);
+	public void restAssuredTestB() {
+		String url = getProducerURL() + REQUEST_AUTH_ENDPOINT;
+		String user = "usernameB";
+		String passwordHash = "afeDWQweqdd124";
+
+		RestAssured
+				.given()
+				.param(REQUEST_USER_PARAM, user)
+				.param(REQUEST_PASSWORD_HASH_PARAM, passwordHash)
+				.get("http://" + url)
+				.then()
+				.statusCode(200);
+		List<Integer> savedIds = postgresUtil.getIDs(user, passwordHash);
+		assertEquals(1, savedIds.size());
+		assertEquals(0, savedIds.get(0));
+
+	}
+
+	public void uglyWait() {
+		try {
+			Thread.sleep(10000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@AfterEach
+	public void shutDownContainers() {
+		kafka.stop();
+		postgres.stop();
+		producer.stop();
+		consumer.stop();
 	}
 
 }
--- src/test/java/com/deliveryhero/demo/testsuite/util/db/PostgresUtil.java
+++ src/test/java/com/deliveryhero/demo/testsuite/util/db/PostgresUtil.java
@@ -3,12 +3,19 @@ package com.deliveryhero.demo.testsuite.util.db;
 import org.springframework.stereotype.Component;
 
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 
 
 @Component
 public class PostgresUtil {
     private static final String INIT_QUERY = "CREATE TABLE public.login_messages (id integer NULL,\"name\" varchar NULL,password_hash varchar NULL,login_ts varchar NULL);";
+    private static final String SELECT_QUERY_FORMAT = "select id\n" +
+            "from login_messages \n" +
+            "where name = '%s'\n" +
+            "and password_hash = '%s';";
+    //private static final
 
     private String url;
     private String username;
@@ -29,5 +36,25 @@ public class PostgresUtil {
         } catch (SQLException ex) {
             System.out.println(ex.getMessage() + "\n" + ex);
         }
+    }
+
+    public List<Integer> getIDs (String name, String passwordHash) {
+        String query = String.format(SELECT_QUERY_FORMAT, name, passwordHash);
+        List<Integer> results = new ArrayList<>();
+
+        try {
+            Connection con = DriverManager.getConnection(url, username, password);
+            Statement st = con.createStatement();
+            ResultSet rs = st.executeQuery(query);
+            while (rs.next())
+            {
+                results.add(rs.getInt(1));
+            }
+            rs.close();
+            st.close();
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage() + "\n" + ex);
+        }
+        return results;
     }
 }
